c6cd8bac0ac2d2d35989781472b01c4497704840
MaxBarroso <maxmilla@gmail.com>
2021-11-16 10:20:12 -0300

    Exercício resolvido.
    Deu trabalho pra lembrar como se utiliza o {} .format

--- Python Mundo 01/Desafio 005.py
+++ Python Mundo 01/Desafio 005.py
@@ -2,3 +2,15 @@
 # Desafio 05 Aula #07 Operadores aritiméticos
 # Faça um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor.
 # Aluno MaxBarroso
+
+print ('Este programa vai mostrar o sucessor e o antecessor do número que você digitar.')
+
+n1 = int( input('Digite um numero: '))
+A = n1 - 1
+S = n1 + 1
+
+print('O Antecessor de ', n1, 'é', A, 'e o sucessor é: ', S )
+
+print('Agora mostrando o mesmo resultado usando a função format:')
+print('O Antecessor de  {}, é: {}, e o Sucessor é:  {} ' .format(n1, A, S))
+
