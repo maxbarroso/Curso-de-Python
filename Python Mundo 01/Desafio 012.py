14cdf9a0786d1929f379a2b4a8fc49f430769b56
MaxBarroso <maxmilla@gmail.com>
2021-11-16 09:44:58 -0300

    exercício adicionados

--- Python Mundo 01/Desafio 012.py
+++ Python Mundo 01/Desafio 012.py
@@ -1,7 +1,5 @@
 # Curso de Python do Curso em Vídeo
 # Desafio 05 Aula #07 Operadores aritiméticos
- # Faça um algoritimo que leia o preço de um protudo e mostre seu novo 
-# preço com 5% de desconto.
+# Faça um algorítimo que leia o preço de um produto e mostre deu novo preço com 5% de desconto.
 
 # Aluno MaxBarroso
-
---
+++ Python Mundo 01/Desafio 013.py
@@ -0,0 +1,5 @@
+# Curso de Python do Curso em Vídeo
+# Desafio 05 Aula #07 Operadores aritiméticos
+# Faça um algoritimo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento# preço com 5% de desconto.
+
+# Aluno MaxBarroso
